f9ca3952eb64e825806e68628d28ca66b7180300
ryu.higuchi <rvcgh9@gmail.com>
2024-01-28 16:29:17 +0900

    Create network resources

--- network.tf
+++ network.tf
@@ -1,3 +1,114 @@
 resource "aws_vpc" "vpn_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name"    = "${var.project_name}-vpc"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_network_acl" "network_acl" {
+  vpc_id = aws_vpc.vpn_vpc.id
+
+  tags = {
+    "Name"    = "${var.project_name}-network-acl"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  vpc_id            = aws_vpc.vpn_vpc.id
+  cidr_block        = var.public_subnet_cidr_blocks[count.index]
+  availability_zone = var.availability_zones[count.index%3]
+
+  tags = {
+    "Name"    = "${var.project_name}-public-subnet-${count.index}"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpn_vpc.id
+
+  tags = {
+    "Name"    = "${var.project_name}-ig"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_route_table" "public_subnet_route_table" {
+  vpc_id = aws_vpc.vpn_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+
+  tags = {
+    "Name"    = "${var.project_name}-public-subnet-rt"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  count          = length(var.public_subnet_cidr_blocks)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_subnet_route_table.id
+}
+
+resource "aws_subnet" "private_subnets" {
+  count = length(var.private_subnet_cidr_blocks)
+
+  vpc_id            = aws_vpc.vpn_vpc.id
+  cidr_block        = element(var.private_subnet_cidr_blocks, count.index)
+  availability_zone = element(var.availability_zones, count.index%3)
+
+  tags = {
+    "Name"    = "${var.project_name}-private-subnet-${count.index}"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_eip" "nat_gateway_ip" {
+  count = length(var.private_subnet_cidr_blocks)
+
+  tags = {
+    "Name"    = "${var.project_name}-eip-${count.index}"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  count = length(var.private_subnet_cidr_blocks)
+
+  subnet_id     = element(var.public_subnet_cidr_blocks, count.index)
+  allocation_id = element(aws_eip.nat_gateway_ip, count.index)
+
+  tags = {
+    "Name"    = "${var.project_name}-nat-gateway-${count.index}"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_route_table" "private_subnet_route_table" {
+  count  = length(var.private_subnet_cidr_blocks)
+  vpc_id = aws_vpc.vpn_vpc.id
+
+  route {
+    cidr_block     = element(var.private_subnet_cidr_blocks, count.index)
+    nat_gateway_id = element(aws_nat_gateway.nat_gateway[*].id, count.index)
+  }
+
+  tags = {
+    "Name"    = "${var.project_name}-private-subnet-rt"
+    "Project" = var.project_name
+  }
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  count          = length(var.private_subnet_cidr_blocks)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.private_subnet_route_table.id
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,24 @@
+variable "project_name" {
+  type    = string
+  default = "private-vpn"
+}
+
 variable "region" {
   type    = string
   default = "ap-northeast-1"
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+}
+
+variable "public_subnet_cidr_blocks" {
+  type    = list(string)
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "private_subnet_cidr_blocks" {
+  type    = list(string)
+  default = ["10.0.10.0/24", "10.0.10.0/24"]
 }
